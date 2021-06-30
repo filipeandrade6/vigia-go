6119c55fb00933eda17e8cabc93320942c3cf1af
Filipe Andrade <filipe.engenharia42@gmail.com>
2021-06-30 20:56:58 -0300

    struturando

--- internal/messages/README.md
+++
@@ -1,3 +0,0 @@
-export PATH="$PATH:$(go env GOPATH)/bin"
-
-protoc -I=./internal/messages --go_out=. --go-grpc_out=. ./internal/messages/*.proto
---
+++ scripts/proto-gen.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PATH="$PATH:$(go env GOPATH)/bin"
+protoc -I=./api/proto/v1 --go_out=. --go-grpc_out=. ./api/proto/v1/*.proto
+#protoc -I=./internal/messages --go_out=. --go-grpc_out=. ./internal/messages/*.proto
