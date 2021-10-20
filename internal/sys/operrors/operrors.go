e7c435b9c3efe8dc2cb52fdb4e66e2a3694ac5fa
Filipe Andrade <filipe.engenharia42@gmail.com>
2021-10-19 21:12:27 -0300

    implementnig custom error

--- internal/sys/operrors/operrors.go
+++ internal/sys/operrors/operrors.go
@@ -1,37 +1,67 @@
 package operrors
 
-type OpErr int
+import "errors"
 
-const (
-	InitNetSDK = iota
-	Unauthorized
-	Unreachable
-	BadRequest
-	NetSDKLogin
-	SaveImage
-	Analyzer
-	NotDahua
+// type OpErr int
+
+// const (
+// 	InitNetSDK = iota
+// 	Unauthorized
+// 	Unreachable
+// 	BadRequest
+// 	NetSDKLogin
+// 	SaveImage
+// 	Analyzer
+// 	NotDahua
+// )
+
+// func (o OpErr) String() string {
+// 	return [...]string{
+// 		"Unauthorized",
+// 		"Unreachable",
+// 		"BadRequest",
+// 		"NetSDKLogin",
+// 		"SaveImage",
+// 		"Analyzer",
+// 		"NotDahua",
+// 	}[o]
+// }
+
+var (
+	ErrInitNetSDK   = errors.New("init netsdk failed")
+	ErrUnauthorized = errors.New("unauthorized")
+	ErrUnreachable  = errors.New("unreachable")
+	ErrBadRequest   = errors.New("bad request")
+	ErrNetSDKLogin  = errors.New("netsdk login failed")
+	ErrSaveImage    = errors.New("could not save image")
+	ErrAnalyzer     = errors.New("analyzer error")
+	ErrNotDahua     = errors.New("not dahua")
 )
-
-func (o OpErr) String() string {
-	return [...]string{
-		"Unauthorized",
-		"Unreachable",
-		"BadRequest",
-		"NetSDKLogin",
-		"SaveImage",
-		"Analyzer",
-		"NotDahua",
-	}[o]
-}
 
 type OpError struct {
 	ServidorID string
 	ProcessoID string
 	RegistroID string
-	Err        OpErr
+	// Err        OpErr
+	Err error
 }
 
-func (o *OpError) Error() string {
+func (o *OpError) Unwrap() error { return o.Err }
 
+func (o *OpError) Error() string {
+	// if o == nil {
+	// 	return "<nil>"
+	// }
+
+	s := "srv[" + o.ServidorID + "]"
+	if o.ProcessoID != "" {
+		s += " prc[" + o.ProcessoID + "]"
+	}
+	if o.RegistroID != "" {
+		s += " reg[" + o.RegistroID + "]"
+	}
+	// s += ": " + o.Err.String()
+	s += ": " + o.Err.Error()
+
+	return s
 }
